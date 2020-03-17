741d67714755d14d8ecc64fc36050cee98132a17
Steven Vergenz <vergenzs@gmail.com>
2020-03-17 15:48:54 -0700

    Remember playing state

--- src/app.ts
+++ src/app.ts
@@ -7,30 +7,34 @@ import { resolve } from 'path';
 
 // https://17853.live.streamtheworld.com/KUOWFM_HIGH_MP3.mp3?dist=kuow
 
+/** Global state, persists after session ends. */
+let playing = false;
+
 /**
  * A simple music player for Street Hoops World in Altspace.
  * Users can play/pause a looping track.
  * Author: Adrian Balanon Audio By: Megan Frazier
  */
 export default class NprRadio {
-	private assets: MRE.AssetContainer;
+	private kuow: MRE.VideoStream;
 	private radioBody: MRE.Actor;
 	private wooferAnim: MRE.Animation;
 	private playObj: MRE.Actor;
+	private vidSettings: MRE.SetVideoStateOptions = { visible: false, paused: false, volume: 0.8, spread: 0 };
 
 	constructor(private context: MRE.Context, private baseUrl: string) {
 		// create asset container for audio
-		this.assets = new MRE.AssetContainer(this.context);
 		this.context.onStarted(() => this.started());
 	}
 
 	// Once the context is "started", initialize the app.
 	private async started() {
-		const kuow = this.assets.createVideoStream("KUOW", {
+		const assets = new MRE.AssetContainer(this.context);
+		this.kuow = assets.createVideoStream("KUOW", {
 			uri: 'https://17853.live.streamtheworld.com/KUOWFM_HIGH_MP3.mp3?dist=kuow'
 		});
 
-		this.radioBody = MRE.Actor.CreateFromGltf(this.assets, {
+		this.radioBody = MRE.Actor.CreateFromGltf(assets, {
 			uri: `${this.baseUrl}/boombox.glb`,
 			colliderType: 'box',
 			actor: {
@@ -43,7 +47,6 @@ export default class NprRadio {
 		this.wooferAnim = this.radioBody.animationsByName.get("boombox_woofer_Action");
 		this.wooferAnim.wrapMode = MRE.AnimationWrapMode.Loop;
 
-		const vidSettings: MRE.SetVideoStateOptions = { visible: false, paused: false, volume: 0.8, spread: 0 };
 		this.radioBody.setBehavior(MRE.ButtonBehavior)
 			.onHover('enter', user => {
 				if (!/moderator/u.test(user.properties['altspacevr-roles'])) return;
@@ -59,17 +62,24 @@ export default class NprRadio {
 			})
 			.onClick(user => {
 				if (!/moderator/u.test(user.properties['altspacevr-roles'])) return;
-
-				if (!this.playObj) {
-					this.playObj = MRE.Actor.Create(this.context, { actor: { name: "PlayObject" }});
-					this.playObj.startVideoStream(kuow.id, vidSettings);
-					this.wooferAnim.play();
-				} else {
-					this.playObj.destroy();
-					this.playObj = null;
-					this.wooferAnim.stop();
-				}
+				this.setPlaying(!playing);
 			});
+
+		this.setPlaying(playing);
+	}
+
+	private setPlaying(on: boolean) {
+		if (on) {
+			this.playObj = MRE.Actor.Create(this.context, { actor: { name: "PlayObject" }});
+			this.playObj.startVideoStream(this.kuow.id, this.vidSettings);
+			this.wooferAnim.play();
+			playing = true;
+		} else {
+			this.playObj.destroy();
+			this.playObj = null;
+			this.wooferAnim.stop();
+			playing = false;
+		}
 	}
 }
 
